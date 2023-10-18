3dff92ef9d729257d034a0ac57148cbad0b46f9d
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-18 14:36:47 -0500

    Java Doc tags

--- src/main/java/useless/terrainapi/api/TerrainAPI.java
+++ src/main/java/useless/terrainapi/api/TerrainAPI.java
@@ -1,6 +1,13 @@
 package useless.terrainapi.api;
 
 public interface TerrainAPI {
+	/**
+	 * @return Mod ID
+	 */
 	String getModID();
+
+	/**
+	 * This method is run by TerrainAPI at startup
+	 */
 	void onInitialize();
 }
