e7eda1f8b9e4ba9647104e4aef9da77adf551df7
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-09 17:21:49 -0600

    Structure Features javadoc

--- src/main/java/useless/terrainapi/generation/StructureFeatures.java
+++ src/main/java/useless/terrainapi/generation/StructureFeatures.java
@@ -7,9 +7,9 @@ import java.util.function.Function;
 public class StructureFeatures extends GeneratorFeatures{
 	public List<Function<Parameters, Void>> featureFunctionList = new ArrayList<>();
 	public List<Object[]> featureParametersList = new ArrayList<>();
-	/** The Object[] are the parameters passed into the provided function, index 0 will always be populated by Biome, index 1 with Random, index 2 with Chunk, index 3 with the ChunkDecorator, and index 4 with the oreHeightModifier. Additional parameters can be added in the method.
-	 * Range Modifier of -1 indicates that the feature should only generate on the surface
-	 *
+	/**Adds a structure entry
+	 * @param function Function that takes a Parameters object and handles the generation
+	 * @param additionalParameters Object[] of additional parameters that will be included with the Parameters object passed into the supplier
 	 */
 	public void addStructure(Function<Parameters, Void> function, Object[] additionalParameters){
 		featureFunctionList.add(function);
