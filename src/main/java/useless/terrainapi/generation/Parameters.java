204129290186e6957444d05c2b361256f4595147
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-09 20:06:36 -0600

    Utilities Docs

--- src/main/java/useless/terrainapi/generation/Parameters.java
+++ src/main/java/useless/terrainapi/generation/Parameters.java
@@ -27,16 +27,4 @@ public class Parameters {
 		this(baseParameter.biome, baseParameter.random, baseParameter.chunk, baseParameter.decorator);
 		this.customParameters = customParameters;
 	}
-
-	public static <T> T[] concatenate(T[] a, T[] b) {
-		int aLen = a.length;
-		int bLen = b.length;
-
-		@SuppressWarnings("unchecked")
-		T[] c = (T[]) Array.newInstance(a.getClass().getComponentType(), aLen + bLen);
-		System.arraycopy(a, 0, c, 0, aLen);
-		System.arraycopy(b, 0, c, aLen, bLen);
-
-		return c;
-	}
 }
